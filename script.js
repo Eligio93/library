3cddb5a48487ab473257516fd8384fb344dd97ae
Eligio93 <eligio.cristantielli@gmail.com>
2023-05-12 00:57:22 +1000

    make the form disappear when click on "add" button

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const newBook=document.getElementById("newbook");
 const form=document.querySelector("form");
+const addBook=document.getElementById("addBook");
 
 
 newBook.addEventListener("click",function(){
     form.style.display="block";
+})
+
+addBook.addEventListener("click",function(event){
+    event.preventDefault();
+    form.style.display="none";
+    
+
 })
